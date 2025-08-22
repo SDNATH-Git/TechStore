04c490fed7e3bfcc009baa5fb138e2e3955b72ff
SDNATH-Git <sd.nath1952@gmail.com>
2025-08-23 03:24:11 +0600

    added a config

--- next.config.js
+++ next.config.js
@@ -1,15 +1,3 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   output: 'export',
-//   eslint: {
-//     ignoreDuringBuilds: true,
-//   },
-//   images: { unoptimized: true },
-// };
-
-// module.exports = nextConfig;
-
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
